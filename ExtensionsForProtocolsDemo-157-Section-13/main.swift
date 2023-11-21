6d90bff45c3ca048ad0ac43a4480be8d78f96a66
Abdurahman AYDIN <hariysu@gmail.com>
2023-11-21 13:59:11 +0300

    Initial commit

--- ExtensionsForProtocolsDemo-157-Section-13/main.swift
+++ ExtensionsForProtocolsDemo-157-Section-13/main.swift
@@ -5,7 +5,66 @@
 //  Created by Abdurahman on 19.11.2023.
 //
 
-import Foundation
+protocol CanFly {
+    // Func body is empty here. It should be defined in class method.
+    func fly()
+}
 
-print("Hello, World!")
+// Bird class'ına fly metodu ekleyerek inheritance ile yapabileceğimiz şeyi, protokol'ün extension'ı ile yaptık.
+// What we could do by adding a fly method to the Bird class, we did with the extension of the protocol.
+extension CanFly{
+    func fly(){
+        print("The object takes off into the air")
+    }
+}
+
+class Bird {
+    var isFemale = true
+    
+    func layEgg(){
+        if(isFemale){
+            print("The bird makes a new bird in a shell")
+        }
+    }
+}
+
+class Eagle: Bird, CanFly {
+//    func fly() {
+//        print("The eagle flaps its wings and lifts off into the sky")
+//    }
+    
+    func soar(){
+        print("The eagle glides in the air using air currents")
+    }
+}
+
+class Penguin: Bird {
+    func swim(){
+        print("The penguin paddles through the water")
+    }
+}
+
+struct FlyingMuseum {
+    // We can use protocol as a dataType(CanFly).
+    func flyingDemo(flyingObject: CanFly){
+        flyingObject.fly()
+    }
+}
+
+struct Airplane: CanFly {
+//    func fly() {
+//        print("The airplane uses its engine to lift off into the air")
+//    }
+}
+
+let myEagle = Eagle()
+myEagle.fly()
+let myPlane = Airplane()
+myPlane.fly()
+
+
+let museum = FlyingMuseum()
+// We can pass object(myPlane) instead of protocol
+museum.flyingDemo(flyingObject: myPlane)
+
 
